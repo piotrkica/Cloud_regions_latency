d53fdb7f64273dcd8f27caee8b9f80a14d3eaf6b
mrzysztof <krzysztof.maliszewski9@wp.pl>
2023-05-13 19:06:40 +0200

    pinger

--- src/azure_demo.py
+++ src/azure_demo.py
@@ -1,11 +1,16 @@
 from azure_provider.regions import all_regions
 from azure_provider.provisioner import AzureProvisioner
+from pinger import pinger
 
 # can be passed all regions at once, then creating vms should be faster
 azure_provisioner = AzureProvisioner(all_regions[:2])
 # azure_provisioner.emergency_cleanup()
 azure_provisioner.init_region_resources()
-print(list(map(lambda x: str(x), azure_provisioner.create_vms(all_regions[:2]))))
+vms = azure_provisioner.create_vms(all_regions[:2])
+
+# returns grid with ping statistics grouped by vm ids
+print(pinger.ping(vms))
+
 input("Press enter to cleanup")
 # clean only some vms, might be useful if we hit some resource limit
 azure_provisioner.cleanup_vms(all_regions[:1])
