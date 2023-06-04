0ca6a2fcc2ccc60f7c916cc515c9e9d586dfa005
Piotrek <piotr.kica.35@gmail.com>
2023-06-04 16:54:41 +0200

    experiment setup, updated valid regions, visualization of results

--- src/azure_demo.py
+++ src/azure_demo.py
@@ -1,17 +1,25 @@
+import os
+import json
+
 from azure_provider.regions import all_regions
 from azure_provider.provisioner import AzureProvisioner
 from pinger import pinger
 
 # can be passed all regions at once, then creating vms should be faster
-azure_provisioner = AzureProvisioner(all_regions[:2])
+azure_provisioner = AzureProvisioner(all_regions)
 # azure_provisioner.emergency_cleanup()
 azure_provisioner.init_region_resources()
-vms = azure_provisioner.create_vms(all_regions[:2])
+vms = azure_provisioner.create_vms(all_regions)
 
 # returns grid with ping statistics grouped by vm ids
-print(pinger.ping(vms))
+pingresults = pinger.ping(vms)
+
+print(pingresults)
+
+with open("pingresults.json", "w+") as f:
+    json.dump(pingresults, f, indent=4)
 
 input("Press enter to cleanup")
 # clean only some vms, might be useful if we hit some resource limit
-azure_provisioner.cleanup_vms(all_regions[:1])
+azure_provisioner.cleanup_vms(all_regions)
 azure_provisioner.cleanup()
--- src/pinger/pinger.py
+++ src/pinger/pinger.py
@@ -1,6 +1,10 @@
 from paramiko import SSHClient, AutoAddPolicy
 from azure_provider.vm_manager import VmDetails
 from re import findall
+from tqdm import tqdm
+
+VmId = str
+PingStatisticsGrid = dict[VmId, dict[VmId, dict]]
 
 
 class PingStatistics:
@@ -11,31 +15,32 @@ class PingStatistics:
         self.mdev = mdev
 
     def __repr__(self):
-       return f'PingStatistics({self.mini}, {self.avg}, {self.maxi}, {self.mdev})' 
+        return {"min": self.mini, "avg": self.avg, "max": self.maxi, "mdev": self.mdev}  # in ms
+
+    def __str__(self):
+        return str({"min": self.mini, "avg": self.avg, "max": self.maxi, "mdev": self.mdev})
 
     @staticmethod
     def from_ping_output(output: str):
         mini, avg, maxi, mdev = findall("\d*\.\d*", output)
-        return PingStatistics(float(mini), float(avg), float(maxi), float(mdev))
+        return {"min": float(mini), "avg": float(avg), "max": float(maxi), "mdev": float(mdev)}
 
-VmId = str
-PingStatisticsGrid = dict[VmId, dict[VmId, PingStatistics]] 
 
-def ping(vms: list[VmDetails], ping_count=10) -> PingStatisticsGrid:
+def ping(vms: list[VmDetails], ping_count=5) -> PingStatisticsGrid:
     result = {vm.id: {} for vm in vms}
 
-    for src_vm in vms:
+    for src_vm in tqdm(vms):
         client = SSHClient()
         client.set_missing_host_key_policy(AutoAddPolicy())
         client.connect(src_vm.ip, username=src_vm.login, password=src_vm.password)
 
-        for dest_vm in vms:
+        for i, dest_vm in enumerate(vms):
             if src_vm.ip != dest_vm.ip:
                 ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command(f"ping -qc {ping_count} {dest_vm.ip}")
                 rtt_stats = ssh_stdout.readlines()[-1]
                 stats = PingStatistics.from_ping_output(rtt_stats)
                 result[src_vm.id][dest_vm.id] = stats
-
+                print(f"Pinged: {src_vm} to {dest_vm}. Completed {i}/{len(vms) - 1}")
         client.close()
-    
+
     return result
